fd232643ae004de57f06f2a0dfbfab84384f02f0
camur <camur@indra.es>
2023-03-23 16:52:32 +0100

    update: Refactor project

--- src/index.js
+++ src/index.js
@@ -5,19 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const startApp = () => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-};
-
-if (window.cordova) {
-  document.addEventListener('deviceready', () => {startApp (); }, false);
-} else {
-  startApp ();
-}
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
